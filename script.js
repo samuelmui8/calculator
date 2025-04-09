3a8ae6896d15897846e96ecce155728510fd4a41
samuelmui8 <samuelmui8@gmail.com>
2025-04-09 13:55:42 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(x, y) {
+	return x + y;
+}
+
+function subtract(x, y) {
+	return x - y;
+}
+
+function multiply(x, y) {
+	return x * y;
+}
+
+function divide(x, y) {
+	return x / y;
+}
+
+function operate(leftNum, rightNum, op) {
+	return op(leftNum, rightNum);
+}
+
+let leftNum, rightNum, op;
